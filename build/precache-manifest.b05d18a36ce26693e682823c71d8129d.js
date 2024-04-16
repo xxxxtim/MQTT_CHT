344ceac926cc1fe466c3e3b8611714b85027f01e
xxxxtm <xxxxtim@hotmail.com>
2024-04-16 22:40:10 +0800

    feat: test for cht

---
+++ build/precache-manifest.b05d18a36ce26693e682823c71d8129d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e85f5486986dee4aa86f261b6512af9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e980d287a1a76ebaaea9",
+    "url": "/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "234706dd8469cd842ccb",
+    "url": "/static/css/main.5722bb4b.chunk.css"
+  },
+  {
+    "revision": "e980d287a1a76ebaaea9",
+    "url": "/static/js/2.e7ee8435.chunk.js"
+  },
+  {
+    "revision": "aad3812c5f3a2eb05746ee652bfda8aa",
+    "url": "/static/js/2.e7ee8435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "234706dd8469cd842ccb",
+    "url": "/static/js/main.5744b71d.chunk.js"
+  },
+  {
+    "revision": "f9b2c1640e64f54e0a42",
+    "url": "/static/js/runtime-main.526a7020.js"
+  }
+]);
